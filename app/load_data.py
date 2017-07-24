dcdaebd67c02db4c7a8c4c1b17889b53b2b643f1
Gustavo Montamat <gmontamat@gmail.com>
2017-07-23 21:41:39 -0300

    Rename functions and add load_train

--- app/load_data.py
+++ app/load_data.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 """
-Functions to load CSV files into memory
+Functions to load input files into memory
 """
 
+import os
 import pandas as pd
 
 
-def load(file_path):
+def load_data(file_path):
     return pd.read_csv(file_path, delimiter=',', quotechar='"')
 
 
+def load_train():
+    path = os.path.dirname(os.path.realpath(__file__))
+    return load_data(os.path.join(path, '..', 'input', 'train_1.csv'))
+
 if __name__ == '__main__':
-    train = load('../data/train_1.csv')
+    train = load_train()
