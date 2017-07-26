f2ca873b8d6ddb94e8dc2ae588f54438203f6f34
Gustavo Montamat <gmontamat@gmail.com>
2017-07-26 15:43:13 -0300

    Add code to remove outliers and filter old data

--- app/median_baseline.py
+++ app/median_baseline.py
@@ -5,10 +5,25 @@ Use median as prediction
 Source: https://www.kaggle.com/clustifier/weekend-weekdays/code
 """
 
+import datetime
 import os
+import numpy as np
 import pandas as pd
 
 from load_data import load_train
+
+
+def replace_outliers(df, replace_with=np.nan):
+    cols = list(df)[1:]
+    df = df.assign(q10=df.quantile(0.1, axis=1))
+    df = df.assign(q90=df.quantile(0.9, axis=1))
+    df = df.assign(upper=df['q90'] + 2.0 * (df['q90'] - df['q10']))
+    df = df.assign(lower=df['q10'] - 2.0 * (df['q90'] - df['q10']))
+    for col in cols:
+        df.loc[df[col] < df['lower'], col] = replace_with
+        df.loc[df[col] > df['upper'], col] = replace_with
+    df.drop(['q10', 'q90', 'lower', 'upper'], axis=1, inplace=True)
+    return df
 
 
 def median_week():
@@ -33,5 +48,32 @@ def median_week():
     test[['Id', 'Visits']].to_csv(os.path.join('..', 'output', 'submission_median.csv'), index=False)
 
 
+def median_day(remove_outliers=False, filter_date=None):
+    train = load_train()
+    if remove_outliers:
+        train = replace_outliers(train)
+    train_flattened = pd.melt(
+        train[list(train.columns[-49:]) + ['Page']], id_vars='Page', var_name='date', value_name='Visits'
+    )
+    train_flattened['date'] = train_flattened['date'].astype('datetime64[ns]')
+    if filter_date:
+        train_flattened = train_flattened[train_flattened['date'] >= datetime.datetime.strptime(filter_date, '%Y-%m-%d')]
+    train_flattened['dayofweek'] = (train_flattened.date.dt.dayofweek >= 5).astype(float)
+
+    test = pd.read_csv(os.path.join('..', 'input', 'key_1.csv'))
+    test['date'] = test.Page.apply(lambda a: a[-10:])
+    test['Page'] = test.Page.apply(lambda a: a[:-11])
+    test['date'] = test['date'].astype('datetime64[ns]')
+    test['dayofweek'] = (test.date.dt.dayofweek >= 5).astype(float)
+
+    train_page_per_dow = train_flattened.groupby(['Page', 'dayofweek']).median().reset_index()
+
+    test = test.merge(train_page_per_dow, how='left')
+    test.loc[test.Visits.isnull(), 'Visits'] = 0
+
+    test[['Id', 'Visits']].to_csv(os.path.join('..', 'output', 'submission_median.csv'), index=False)
+
+
 if __name__ == '__main__':
-    median_week()
+    # median_week()
+    median_day(remove_outliers=True)
