b4ce27b4482aff04b06ea46d08a28f1251d89c5d
Gustavo Montamat <gmontamat@gmail.com>
2017-08-21 23:08:32 -0300

    Use pd.melt to flatten train data

--- app/load_data.py
+++ app/load_data.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 """
-Functions to load input files into memory
+Functions to load input files into memory or a database system
 """
 
 import os
 import pandas as pd
+
+from dao import SimpleDao
 
 
 def load_data(file_path):
@@ -17,5 +19,13 @@ def load_train():
     return load_data(os.path.join(app_path, '..', 'input', 'train_1.csv'))
 
 
+def load_flat_train():
+    flat = pd.melt(load_train(), id_vars=['Page'], var_name='Date', value_name='Visits')
+    flat['Date'] = flat['Date'].astype('datetime64[ns]')
+    return flat
+
+
 if __name__ == '__main__':
-    train = load_train()
+    train_flat = load_flat_train()
+    dao = SimpleDao('localhost', '5432', 'postgres', 'postgres', 'kaggle-wttsf')
+    dao.upload_from_dataframe(train_flat, 'train_flat')
