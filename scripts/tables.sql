000ead190a475c79871d056a9aa1e8674a54c5f7
Gustavo Montamat <gmontamat@gmail.com>
2017-09-02 15:42:17 -0300

    Add more lags as features and generate table of test features

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -1,4 +1,6 @@
+--
 -- Split the Page column of the flat train data
+--
 
 DROP TABLE IF EXISTS train_flat_split;
 
@@ -18,13 +20,26 @@ CREATE TABLE train_flat_split AS (
     FROM train_flat
 );
 
+ALTER TABLE train_flat_split
+  ADD PRIMARY KEY (page, date);
 
+
+--
 -- Table of features (visits_lag#, project, access, and agent) and response value (visits)
+--
 
 DROP TABLE IF EXISTS xy;
 
 CREATE TABLE xy AS (
-  SELECT page, project, access, agent, dow, month, date::DATE, visits,
+  SELECT page, name, project, access, agent, dow, month, date::DATE, visits,
+         lag(visits, 1) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag1,
+         lag(visits, 2) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag2,
+         lag(visits, 3) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag3,
+         lag(visits, 4) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag4,
+         lag(visits, 5) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag5,
+         lag(visits, 6) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag6,
+         lag(visits, 7) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag7,
+         lag(visits, 8) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag8,
          lag(visits, 9) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag9,
          lag(visits, 10) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag10,
          lag(visits, 11) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag11,
@@ -118,3 +133,112 @@ CREATE TABLE xy AS (
          lag(visits, 99) OVER (PARTITION BY page ORDER BY date ASC) AS visits_lag99
     FROM train_flat_split
 );
+
+ALTER TABLE xy
+  ADD PRIMARY KEY (page, date);
+
+
+--
+-- Table of test features to predict response
+--
+
+DROP TABLE IF EXISTS testx;
+
+CREATE TABLE testx AS (
+  WITH dates AS (
+     SELECT i::DATE AS date
+       FROM generate_series('2017-01-01', '2017-03-01', '1 day'::INTERVAL) i
+  )
+     SELECT pages.page AS page,
+            reverse(split_part(reverse(pages.page), '_', 3)) AS project,
+            reverse(split_part(reverse(pages.page), '_', 2)) AS access,
+            reverse(split_part(reverse(pages.page), '_', 1)) AS agent,
+            date_part('dow', dates.date) AS dow,
+            date_part('month', dates.date) AS month,
+            dates.date AS date,
+            xy.visits AS visits_lag12,
+            xy.visits_lag1 AS visits_lag13,
+            xy.visits_lag2 AS visits_lag14,
+            xy.visits_lag3 AS visits_lag15,
+            xy.visits_lag4 AS visits_lag16,
+            xy.visits_lag5 AS visits_lag17,
+            xy.visits_lag6 AS visits_lag18,
+            xy.visits_lag7 AS visits_lag19,
+            xy.visits_lag8 AS visits_lag20,
+            xy.visits_lag9 AS visits_lag21,
+            xy.visits_lag10 AS visits_lag22,
+            xy.visits_lag11 AS visits_lag23,
+            xy.visits_lag12 AS visits_lag24,
+            xy.visits_lag13 AS visits_lag25,
+            xy.visits_lag14 AS visits_lag26,
+            xy.visits_lag15 AS visits_lag27,
+            xy.visits_lag16 AS visits_lag28,
+            xy.visits_lag17 AS visits_lag29,
+            xy.visits_lag18 AS visits_lag30,
+            xy.visits_lag19 AS visits_lag31,
+            xy.visits_lag20 AS visits_lag32,
+            xy.visits_lag21 AS visits_lag33,
+            xy.visits_lag22 AS visits_lag34,
+            xy.visits_lag23 AS visits_lag35,
+            xy.visits_lag24 AS visits_lag36,
+            xy.visits_lag25 AS visits_lag37,
+            xy.visits_lag26 AS visits_lag38,
+            xy.visits_lag27 AS visits_lag39,
+            xy.visits_lag28 AS visits_lag40,
+            xy.visits_lag29 AS visits_lag41,
+            xy.visits_lag30 AS visits_lag42,
+            xy.visits_lag31 AS visits_lag43,
+            xy.visits_lag32 AS visits_lag44,
+            xy.visits_lag33 AS visits_lag45,
+            xy.visits_lag34 AS visits_lag46,
+            xy.visits_lag35 AS visits_lag47,
+            xy.visits_lag36 AS visits_lag48,
+            xy.visits_lag37 AS visits_lag49,
+            xy.visits_lag38 AS visits_lag50,
+            xy.visits_lag39 AS visits_lag51,
+            xy.visits_lag40 AS visits_lag52,
+            xy.visits_lag41 AS visits_lag53,
+            xy.visits_lag42 AS visits_lag54,
+            xy.visits_lag43 AS visits_lag55,
+            xy.visits_lag44 AS visits_lag56,
+            xy.visits_lag45 AS visits_lag57,
+            xy.visits_lag46 AS visits_lag58,
+            xy.visits_lag47 AS visits_lag59,
+            xy.visits_lag48 AS visits_lag60,
+            xy.visits_lag49 AS visits_lag61,
+            xy.visits_lag50 AS visits_lag62,
+            xy.visits_lag51 AS visits_lag63,
+            xy.visits_lag52 AS visits_lag64,
+            xy.visits_lag53 AS visits_lag65,
+            xy.visits_lag54 AS visits_lag66,
+            xy.visits_lag55 AS visits_lag67,
+            xy.visits_lag56 AS visits_lag68,
+            xy.visits_lag57 AS visits_lag69,
+            xy.visits_lag58 AS visits_lag70,
+            xy.visits_lag59 AS visits_lag71,
+            xy.visits_lag60 AS visits_lag72,
+            xy.visits_lag61 AS visits_lag73,
+            xy.visits_lag62 AS visits_lag74,
+            xy.visits_lag63 AS visits_lag75,
+            xy.visits_lag64 AS visits_lag76,
+            xy.visits_lag65 AS visits_lag77,
+            xy.visits_lag66 AS visits_lag78,
+            xy.visits_lag67 AS visits_lag79,
+            xy.visits_lag68 AS visits_lag80,
+            xy.visits_lag69 AS visits_lag81,
+            xy.visits_lag70 AS visits_lag82,
+            xy.visits_lag71 AS visits_lag83,
+            xy.visits_lag72 AS visits_lag84,
+            xy.visits_lag73 AS visits_lag85,
+            xy.visits_lag74 AS visits_lag86,
+            xy.visits_lag75 AS visits_lag87,
+            xy.visits_lag76 AS visits_lag88,
+            xy.visits_lag77 AS visits_lag89,
+            xy.visits_lag78 AS visits_lag90,
+            xy.visits_lag79 AS visits_lag91
+       FROM (SELECT DISTINCT page FROM train_flat_split) pages, dates
+  LEFT JOIN xy ON xy.page=pages.page AND xy.date-12=dates.date
+);
+
+ALTER TABLE testx
+  ADD PRIMARY KEY (page, date);
